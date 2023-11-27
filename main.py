bafd10b4b46cef08e71a7fb580e5414224216d4e
saharshachar <saharshachar@gmail.com>
2023-11-27 21:35:35 +0200

    2st commit to master

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("main1")
+    print("main2")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
